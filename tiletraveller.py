58c3dd3beaf995d5990c5d026b8acae6d4807431
Dram45 <stefano10@ru.is>
2018-09-21 20:49:43 +0000

    Made loops and base code

--- tiletraveller.py
+++ tiletraveller.py
@@ -5,13 +5,38 @@
 #if move is valid, move there and show new options.
 #gitrepository = https://github.com/Dram45/Tiletraveller.git
 
-MINX = 1, MAXX = 3
-MINY = 1, MAXY = 3
-
+MINX = 1
+MAXX = 3
+MINY = 1 
+MAXY = 3
+victory = False
 currentpositionx = 1
 currentpositiony = 1
-
 validmoves = {'n'}
+validmoveslength = len(validmoves)
+print(validmoveslength)
+
+move = input("Direction: ")
+move = move.lower()
+
+allowedmove = False
+while allowedmove == False:
+    if move not in validmoves:
+        print("Not a valid direction")
+        move = input("Direction: ")
+    if move in validmoves:
+        allowedmove = True
+while victory == False:
+    if move == 'n':
+        currentpositionx += 1
+    if move == 's':
+        currentpositionx -= 1
+    if move == 'w':
+        currentpositiony -= 1
+    if move == 'e':
+        currentpositiony += 1
+    
+
 if currentpositionx == 1 and currentpositiony == 1:
     validmoves = {'n'}
 if currentpositionx == 1 and currentpositiony == 2:
@@ -25,11 +50,8 @@ if currentpositionx == 2 and currentpositiony == 2:
 if currentpositionx == 2 and currentpositiony == 3:
     validmoves = {'w','e'}
 if currentpositionx == 3 and currentpositiony == 1:
-    print("Victory")
-    break
+    victory = True
 if currentpositionx == 3 and currentpositiony == 2:
     validmoves = {'n', 's'}
 if currentpositionx == 3 and currentpositiony == 3:
     validmoves = {'w', 's'}
-
-
