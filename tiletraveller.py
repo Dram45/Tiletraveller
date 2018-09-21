ded20d089b211658e19c9f8982f2356ceec32f4c
Dram45 <stefano10@ru.is>
2018-09-21 21:25:57 +0000

    Code seems to work implemented all the bits into loop, swapped x and y values in wall list had it set up wrong

--- tiletraveller.py
+++ tiletraveller.py
@@ -12,44 +12,41 @@ MAXY = 3
 victory = False
 currentpositionx = 1
 currentpositiony = 1
-validmoves = ['n','s','w','e']
-validmoveslength = len(validmoves)
-print(validmoveslength)
-
-
-mainstring = str("You can travel: ")
-counter = 0
-for chars in validmoves:
-    if counter >= 1 and counter < len(validmoves):
-        mainstring = mainstring + " or "
-    if chars == 'n':
-        mainstring = mainstring + "(N)orth"
-    elif chars == 's':
-        mainstring = mainstring + "(S)outh"
-    elif chars == 'e':
-        mainstring = mainstring + "(E)ast"
-    elif chars == 'w':
-        mainstring = mainstring + "(W)est"
-    counter += 1
-mainstring = mainstring + "."
-
-print(mainstring)
-mainstring = str("You can travel: ")
-        
+validmoves = ['n']
 
 
 
-move = input("Direction: ")
-move = move.lower()
-
-allowedmove = False
-while allowedmove == False:
-    if move not in validmoves:
-        print("Not a valid direction")
-        move = input("Direction: ")
-    if move in validmoves:
-        allowedmove = True
 while victory == False:
+    mainstring = str("You can travel: ")
+    counter = 0
+    for chars in validmoves:
+        if counter >= 1 and counter < len(validmoves):
+            mainstring = mainstring + " or "
+        if chars == 'n':
+            mainstring = mainstring + "(N)orth"
+        elif chars == 's':
+            mainstring = mainstring + "(S)outh"
+        elif chars == 'e':
+            mainstring = mainstring + "(E)ast"
+        elif chars == 'w':
+            mainstring = mainstring + "(W)est"
+        counter += 1
+    mainstring = mainstring + "."
+
+    print(mainstring)
+    mainstring = str("You can travel: ")
+
+    move = input("Direction: ")
+    move = move.lower()
+
+    allowedmove = False
+    while allowedmove == False:
+        if move not in validmoves:
+            print("Not a valid direction")
+            move = input("Direction: ")
+        if move in validmoves:
+            allowedmove = True
+
     if move == 'n':
         currentpositionx += 1
     if move == 's':
@@ -57,24 +54,26 @@ while victory == False:
     if move == 'w':
         currentpositiony -= 1
     if move == 'e':
-        currentpositiony += 1
-    
+        currentpositiony += 1    
 
-if currentpositionx == 1 and currentpositiony == 1:
-    validmoves = ['n']
-if currentpositionx == 1 and currentpositiony == 2:
-    validmoves = ['n','s','e']
-if currentpositionx == 1 and currentpositiony == 3:
-    validmoves =  ['s', 'e']
-if currentpositionx == 2 and currentpositiony == 1:
-    validmoves = ['n']
-if currentpositionx == 2 and currentpositiony == 2:
-    validmoves = ['w','s']
-if currentpositionx == 2 and currentpositiony == 3:
-    validmoves = ['w','e']
-if currentpositionx == 3 and currentpositiony == 1:
-    victory = True
-if currentpositionx == 3 and currentpositiony == 2:
-    validmoves = ['n', 's']
-if currentpositionx == 3 and currentpositiony == 3:
-    validmoves = ['w', 's']
+    if currentpositiony == 1 and currentpositiony == 1:
+        validmoves = ['n']
+    if currentpositiony == 1 and currentpositionx == 2:
+        validmoves = ['n','s','e']
+    if currentpositiony == 1 and currentpositionx == 3:
+        validmoves =  ['s', 'e']
+    if currentpositiony == 2 and currentpositionx == 1:
+        validmoves = ['n']
+    if currentpositiony == 2 and currentpositionx == 2:
+        validmoves = ['w','s']
+    if currentpositiony == 2 and currentpositionx == 3:
+        validmoves = ['w','e']
+    if currentpositiony == 3 and currentpositionx == 1:
+        victory = True
+        break
+    if currentpositiony == 3 and currentpositionx == 2:
+        validmoves = ['n', 's']
+    if currentpositiony == 3 and currentpositionx == 3:
+        validmoves = ['w', 's']
+
+print("Victory!")
