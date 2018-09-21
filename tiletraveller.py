c69c129de9e0d16be5b8fbe0cf869d8b0a609961
Dram45 <stefano10@ru.is>
2018-09-21 20:05:26 +0000

    first commit

---
+++ tiletraveller.py
@@ -0,0 +1,5 @@
+#Tiletraveller
+#Travel within 3x3 grid with several paths walled off
+#As for possible input if starting in spot 1.1 only one path is available
+#show possible paths, check if walls are implemented before showing and if none add that as a move option
+#if move is valid, move there and show new options.
